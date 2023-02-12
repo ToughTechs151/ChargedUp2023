876ab767577e04d0dfec4281601617f20349ed4e
Diya Mahaveer <diyamahaveer@gmail.com>
2023-02-12 12:48:48 -0500

    Assigned Joystick button to arm.

--- src/main/java/frc/robot/commands/ArmDownCommand.java
+++ src/main/java/frc/robot/commands/ArmDownCommand.java
@@ -5,16 +5,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.ArmPIDSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ArmDownCommand extends InstantCommand {
-  public ArmDownCommand() {
+  private ArmPIDSubsystem armSubsystem;
+  public ArmDownCommand(ArmPIDSubsystem arm) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(arm);
+    armSubsystem = arm;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    armSubsystem.down();
+  }
 }
--- src/main/java/frc/robot/commands/ArmUpCommand.java
+++ src/main/java/frc/robot/commands/ArmUpCommand.java
@@ -5,16 +5,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.ArmPIDSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ArmUpCommand extends InstantCommand {
-  public ArmUpCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
+
+  private ArmPIDSubsystem armSubsystem;
+  public ArmUpCommand(ArmPIDSubsystem arm) {
+      // Use addRequirements() here to declare subsystem dependencies.
+      addRequirements(arm);
+      armSubsystem = arm;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    armSubsystem.up();
+  }
 }
