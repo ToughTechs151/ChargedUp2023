9ecc3991d4fe9cf9f41fd1ab994a3acbe901a179
Brian Utterback <brian@utterback.org>
2023-02-22 11:40:41 -0500

    Merge Diya's branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -106,6 +106,51 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     /* Return null if there is no autonomous command to run. */
     return null;
+    // // Create config for trajectory
+    // TrajectoryConfig config =
+    //     new TrajectoryConfig(
+    //             AutoConstants.kMaxSpeedMetersPerSecond,
+    //             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+    //         // Add kinematics to ensure max speed is actually obeyed
+    //         .setKinematics(DriveConstants.kDriveKinematics);
+    // // An example trajectory to follow.  All units in meters.
+    // Trajectory exampleTrajectory =
+    //     TrajectoryGenerator.generateTrajectory(
+    //         // Start at the origin facing the +X direction
+    //         new Pose2d(0, 0, new Rotation2d(0)),
+    //         // Pass through these two interior waypoints, making an 's' curve path
+    //         List.of(new Translation2d(0, 0), new Translation2d(0, 0)),
+    //         // End 3 meters straight ahead of where we started, facing forward
+    //         new Pose2d(3, 0, new Rotation2d(0)),
+    //         config);
+    // MecanumControllerCommand mecanumControllerCommand =
+    //     new MecanumControllerCommand(
+    //         exampleTrajectory,
+    //         m_robotDrive::getPose,
+    //         DriveConstants.kFeedforward,
+    //         DriveConstants.kDriveKinematics,
+    //         // Position contollers
+    //         new PIDController(AutoConstants.kPXController, 0, 0),
+    //         new PIDController(AutoConstants.kPYController, 0, 0),
+    //         new ProfiledPIDController(
+    //             AutoConstants.kPThetaController, 0, 0,
+    // AutoConstants.kThetaControllerConstraints),
+    //         // Needed for normalizing wheel speeds
+    //         AutoConstants.kMaxSpeedMetersPerSecond,
+    //         // Velocity PID's
+    //         new PIDController(DriveConstants.kPFrontLeftVel, 0, 0),
+    //         new PIDController(DriveConstants.kPRearLeftVel, 0, 0),
+    //         new PIDController(DriveConstants.kPFrontRightVel, 0, 0),
+    //         new PIDController(DriveConstants.kPRearRightVel, 0, 0),
+    //         m_robotDrive::getCurrentWheelSpeeds,
+    //         m_robotDrive::setDriveMotorControllersVolts, // Consumer for the output motor
+    // voltages
+    //         m_robotDrive);
+    // // Reset odometry to the starting pose of the trajectory.
+    // m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // // Run path following command, then stop at the end.
+    // return mecanumControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
   }
 
   /**
