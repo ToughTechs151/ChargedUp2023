ac4b422dc5b6386e0b11c89e72e34139a53569eb
Vera Mangahas <vera.mangahas2005@gmail.com>
2023-02-25 16:03:48 -0500

    Edit ArmPIDSubsystem and Commands and tested on Robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -63,6 +64,7 @@ public class RobotContainer {
                     this.driverController.getRightX(),
                     false),
             this.robotDrive));
+    SmartDashboard.putData("ArmSubsystem", armSubsystem);
   }
 
   /**
