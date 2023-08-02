546f589bfad06d1190f02e59c7f1489310d02c5c
Diya Mahaveer <diyamahaveer@gmail.com>
2023-08-02 19:46:42 -0400

    Changed arm_down constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -162,7 +162,7 @@ public final class Constants {
     public static final double ARM_UP_POSITION = 0.0;
     public static final double ARM_SCORE_LOW_POSITION = Math.round(ARM_ROTATION * 70 / 360);
     public static final double ARM_SCORE_HIGH_POSITION = Math.round(ARM_ROTATION * 60 / 360);
-    public static final double ARM_DOWN_POSITION = Math.round(ARM_ROTATION * 125 / 360);
+    public static final double ARM_DOWN_POSITION = Math.round(ARM_ROTATION * 97 / 360);
     public static final double ARM_RED_ZONE = Math.round(ARM_ROTATION * 75 / 360);
   }
 
